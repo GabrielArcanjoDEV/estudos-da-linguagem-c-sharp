be0e2b289ee46e86e07825e83257e0319646959c
Gabriel Arcanjo <gab1655830@gmail.com>
2026-07-22 15:45:17 -0300

    adicionando aula02, Variáveis

---
+++ aula02.cs
@@ -0,0 +1,21 @@
+namespace aula02;
+
+public class program
+{
+    public static void Main()
+    {
+        string myName = "gabrielDev";
+        int number = 20;
+        double numberDouble = 10.5;
+        char character = 'b';
+        bool boolean = true;
+
+    // ------------------------------------------------
+    
+        Console.WriteLine(myName);
+        Console.WriteLine(number);
+        Console.WriteLine(numberDouble);
+        Console.WriteLine(character);
+        Console.WriteLine(boolean);
+    }
+}

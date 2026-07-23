3bf9253c668150e0fea62e4eea52e382bb63d554
Gabriel Arcanjo <gab1655830@gmail.com>
2026-07-23 16:21:25 -0300

    concertando bug final e adicionando aula05

---
+++ obj/Debug/net10.0/Aulas/aula05.cs
@@ -0,0 +1,18 @@
+namespace aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int number1 = 10;
+            int number2 = 20;
+
+            int sum = number1 + number2;
+            Console.WriteLine("O valor da soma é: " + sum);
+
+
+            int multiplicacão = number1 * number2;
+            Console.WriteLine("O valor da multiplicacão é: " + multiplicacão);
+        }
+    }
+}
